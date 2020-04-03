9cc9bcbba23daaf0e16c150fa6352f07b4e267ce
muondu <munenemuondu@gmail.com>
2020-04-03 11:22:58 +0300

    I have made some changes

---
+++ README.md
@@ -0,0 +1,2 @@
+# My project
+This project ask the user to input three measure ments and tells you if the mearsure ments that the user inputed can make a triangle.
--- triangle.py
+++ triangle.py
@@ -1,10 +1,15 @@
 def logical():
+	#The first input
 	input1 = int(input("Enter your length:  "))
+	#The second input
 	input2 = int(input("Enter your length:  "))
+	#The third input
 	input3 = int(input("Enter your length:  "))
 	def is_triangle():
+		#Checks if it can make a triangle
 		if input1**2 + input2**2 == input3**2:
 			print("It is a triangle")
+		#If it cannot make a triangle
 		else:
 			print("It is not a triangle")
 	is_triangle()
