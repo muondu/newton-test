7ef766b05c4e4da4e97bec69e3f8760cc94d8952
muondu <munenemuondu@gmail.com>
2020-04-03 12:17:08 +0300

    I have made some changes

--- triangle.py
+++ triangle.py
@@ -1,16 +1,23 @@
 def logical():
 	#The first input
+	global input1
 	input1 = int(input("Enter your length:  "))
-	#The second input
+	#The second input'
+	global input2
 	input2 = int(input("Enter your length:  "))
 	#The third input
+	global input3
 	input3 = int(input("Enter your length:  "))
-	def is_triangle():
-		#Checks if it can make a triangle
-		if input1**2 + input2**2 == input3**2:
-			print("It is a triangle")
-		#If it cannot make a triangle
-		else:
-			print("It is not a triangle")
-	is_triangle()
+	
 logical()
+
+global input2
+global input3
+def is_triangle():
+	#Checks if it can make a triangle
+	if input1**2 + input2**2 == input3**2:
+		print("It is a triangle")
+	#If it cannot make a triangle
+	else:
+		print("It is not a triangle")
+is_triangle()
