6d595def2ac5cfa2a145c64223e6b1c9d9fe74f5
muondu <munenemuondu@gmail.com>
2020-04-03 09:46:55 +0300

    I have made some changes

--- triangle.py
+++ triangle.py
@@ -1,11 +1,6 @@
-def logical():
-	input1 = int(input("Enter your first length:  "))
-	input2 = int(input("Enter your second length:  "))
-	input3 = int(input("Enter your third length:  "))
-logical()
 def is_triangle(a,b,c):
-	if a + b > c and b + c > a and a + c > b:
+	if a**2 + b**2 == c**2:
 		print("It is a triangle")
 	else:
 		print("It is not a triangle")
-is_triangle(input1,input2,input3)
+is_triangle(3,4,5)
